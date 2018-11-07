e022bdfb3b0125f7e21359edfed5b9985d2d396c
Daniel Hernqvist <daniel.hernqvist@chasacademy.se>
2018-11-07 11:52:47 +0100

    change code for the checkletter function and get the right amount of pictures out of wrong answers

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 // Globala variabler
 var wordList = ["HÄR", "FINNS", "MASSA", "RANDOM", "ORD"]; // Lista med spelets alla ord// Ett av orden valt av en slumpgenerator
 var letterBoxes; //Rutorna där bokstäverna ska stå
-var hangmanImg = ["h0.png", "h1.png", "h2.png", "h3.png", "h4.png", "h5.png", "h6.png"]; //Bild som kommer vid fel svar
 var hangmanImgNr = document.querySelector('img'); // Vilken av bilderna som kommer upp beroende på hur många fel du gjort
 var msgElem; // Ger meddelande när spelet är över
 var startGameBtn = document.getElementById('startGameBtn'); // Knappen du startar spelet med
@@ -9,7 +8,7 @@ var letterButtons = document.querySelectorAll('#letterButtons > li > .btn'); // 
 var startTime = 5; // Mäter tiden
 var selectedWord; // randomizeat ord
 var letterBoxesEl = document.querySelector('#letterBox'); //deklaration på de tomma bokstavslådorna
-
+var hangmanImg = ["images/h0.png", "images/h1.png", "images/h2.png", "images/h3.png", "images/h4.png", "images/h5.png", "images/h6.png"]; //Bild som kommer vid fel svar
 
 
 // Funktion som körs då hela webbsidan är inladdad, dvs då all HTML-kod är utförd
@@ -41,44 +40,37 @@ var printLetters = function () {
 }
 
 // Funktion som körs när du trycker på bokstäverna och gissar bokstav
-var miss = 0;
-const btnClickCallback = function (event) {
-  var checkGuess = event.target.value;
-  // let a = "x";
-  for (let i = 0; i < selectedWord.length; i++) {
-    if (selectedWord[i] === event.target.value) {
-      console.log("rätt");
+var hit = 0;
+var miss = 1;
+function btnClickCallback(event) {
+  event.target.value.disabled = true;
+  for (let i = 0; i < selectedWord.length; i++){
+    if (event.target.value == selectedWord[i].charAt(0)){
       let printSome = document.querySelectorAll('#letterBox');
       printSome[i].innerHTML = '<input type="text" disabled value="' + event.target.value + '"/>';
-      pressedButton = "hit";
-    }
-    else {
-      pressedButton = "miss";
-      console.log("fel");
-      if (selectedWord[i] !== event.target.value){
-        for (x = 1; x < hangmanImg.length; x++)
-          bild = hangmanImg[x];
-      hangmanImgNr.src = "images/h" + miss + ".png";
-      }
+      hit++;
     }
   }
-  if (pressedButton !== "hit") {
-    console.log("miss");
-    miss++
+  if (selectedWord.includes(event.target.value) == false) {
+    miss++;
+    document.querySelector("#hangman").src = hangmanImg[miss];
   }
+  checkWin();
 }
-
-
-
-
 
 for (let i = 0; i < letterButtons.length; i++) {
   letterButtons[i].addEventListener('click', btnClickCallback);
 }
 
-
 // Funktionen ropas vid vinst eller förlust, gör olika saker beroende av det
 
+function checkWin(){
+  if (miss == 6){
+    alert("you loose!");
+  } if (selectedWord.length == hit) {
+    alert("you win!");
+  }
+}
 
 // Funktion som inaktiverar/aktiverar bokstavsknapparna beroende på vilken del av spelet du är på
 
